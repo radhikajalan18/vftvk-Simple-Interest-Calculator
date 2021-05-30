466ea71daa7547d120ea4304deb90178bbc06748
Radhika Jalan <radhika.jalan18@gmail.com>
2021-05-30 15:09:29 +0000

    changes are made

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
     p = document.getElementById("principal").value;
     
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+object.onchange = updateRate{myScript};
+document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+ if(principal<=0)
+ {
+     alert("Enter a positive number");
+ }       
